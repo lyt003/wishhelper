fa3c292a3beeea7607e1f4f77f163005ee28fa3d
yangwu <ydengwu@gmail.com>
2015-10-21 23:24:35 +0800

    add order by yanwen API

--- yanwen.php
+++ yanwen.php
@@ -14,50 +14,50 @@ $post_header = array('Authorization: basic MTA0OTAzOnlhbmd3dTE5ODIxMTEy','Conten
 
 $curl = curl_init();
 //$url = ServiceEndPoint.LoginURL.userid."/".userpsd;
-$url = ServiceEndPoint."/Users/".userid."/GetChannels";
-//$url = ServiceEndPoint."/Users/".userid."/Expresses";
+//$url = ServiceEndPoint."/Users/".userid."/GetChannels";
+$url = ServiceEndPoint."/Users/".userid."/Expresses";
 curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-//curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POST, true);
 curl_setopt($curl,CURLOPT_HTTPHEADER, $post_header); 
 
 
 $xmldata = <<<DATA
 <ExpressType>
-<Epcode></Epcode>
-    <Userid>100000</Userid>
-    <Channel>ÑàÓÊ±¦¹ÒºÅ</Channel>
-    <UserOrderNumber>123456</UserOrderNumber>
-    <SendDate>2014-07-09T00:00:00</SendDate>
+	<Epcode></Epcode>
+    <Userid>104903</Userid>
+    <Channel>154</Channel>
+    <UserOrderNumber>5102196</UserOrderNumber>
+    <SendDate>2015-10-21T22:14:00</SendDate>
     <Receiver>
-        <Userid>100000</Userid>
+        <Userid>104903</Userid>
         <Name>tang</Name>
         <Phone>1236548</Phone>
         <Mobile>802</Mobile>
         <Email>jpcn@mpc.com.br</Email>
         <Company></Company>
-        <Country>¶íÂÞË¹</Country>
+        <Country>RU</Country>
         <Postcode>253400</Postcode>
         <State>FL</State>
         <City>City</City>
         <Address1>String content1</Address1>
         <Address2>String content2</Address2>
-</Receiver>
-<Memo></Memo>
+	</Receiver>
+	<Memo></Memo>
     <Quantity>1</Quantity>
     <GoodsName>
-        <Userid>100000</Userid>
+        <Userid>104903</Userid>
         <NameCh>¶àÃ½Ìå²¥·ÅÆ÷</NameCh>
         <NameEn>MedialPlayer</NameEn>
         <Weight>213</Weight>
         <DeclaredValue>125</DeclaredValue>
         <DeclaredCurrency>USD</DeclaredCurrency>
-<MoreGoodsName>MedialPlayer</MoreGoodsName>
+		<MoreGoodsName>MedialPlayer</MoreGoodsName>
     </GoodsName>
 </ExpressType>
 DATA;
 
-//curl_setopt($curl, CURLOPT_POSTFIELDS, $xmldata);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $xmldata);
 
 $result = curl_exec($curl);
 //$decode_result = json_decode($result);
