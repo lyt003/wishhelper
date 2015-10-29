ef013654621e755dc72f7ad2d008e0ce0c6e6e8d
yangwu <ydengwu@gmail.com>
2015-10-29 14:32:37 +0800

    add index1 and login page

---
+++ index1.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html prefix="og: http://ogp.me/ns#" lang="en">
+
+<head>
+<meta charset="UTF-8">
+<link rel="schema.DC" href="http://purl.org/dc/elements/1.1/">
+<title>wish 订单助手</title>
+<meta name="DC.title" content="index">
+<meta property="og:title" content="index">
+<meta property="og:image"
+	content="/wish/x1uy05/_screenshots/images/thumbnail/1700_1445683494.png_1500x1500.jpeg">
+<meta property="og:type" content="website">
+<meta property="og:site_name" content="FROONT">
+<meta name="DC.language" scheme="ISO639-1" content="en">
+<link rel="stylesheet" href="css/base.css">
+<link rel="stylesheet" href="css/style.css">
+<script type="text/javascript" src="vendor-js/jquery.min.js"></script>
+<script type="text/javascript"
+	src="vendor-js/noframework.waypoints.min.js"></script>
+<script type="text/javascript" src="vendor-js/responsive-nav.min.js"></script>
+<script type="text/javascript" src="vendor-js/idangerous.swiper.min.js"></script>
+<script type="text/javascript" src="js/form-submit.js"></script>
+<script type="text/javascript" src="js/reveal-animation.js"></script>
+<script type="text/javascript" src="js/background-parallax.js"></script>
+<script type="text/javascript" src="js/page-script.js"></script>
+<script type="text/javascript">
+        var fonts = {
+            "Oxygen": {
+                "family": "Oxygen",
+                "source": "google",
+                "subsets": [
+                    "latin",
+                    "latin-ext"
+                ],
+                "variants": [
+                    "300",
+                    "700",
+                    "regular"
+                ]
+            },
+            "europa": {
+                "css_name": "europa",
+                "family": "Europa",
+                "id": "myvg",
+                "source": "typekit",
+                "stack": "sans-serif",
+                "variants": [
+                    "n3",
+                    "i3",
+                    "n4",
+                    "i4",
+                    "n7",
+                    "i7"
+                ]
+            }
+        };
+    </script>
+<script type="text/javascript" src="vendor-js/webfont.js"></script>
+<script type="text/javascript" src="js/fontloader.js"></script>
+<meta http-equiv="content-language" content="en">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+	<div class="b-content">
+		<div id="navigation"
+			class="fr-widget fr-navigation fr-navigation-active fr_navigation">
+			<div id="mobile_toggle" class="fr-widget fr-svg fr_mobile_toggle">
+				<div class="fr-svg-inner">
+					<svg xmlns="http://www.w3.org/2000/svg" version="1.1" x="0" y="0"
+						viewBox="0 0 73.2 61.6" xml:space="preserve">
+                        <path
+							d="M4.2 8.6h64.7c2.3 0 4.2-1.9 4.2-4.3C73.2 1.9 71.3 0 68.9 0H4.2C1.9 0 0 1.9 0 4.3 0 6.7 1.9 8.6 4.2 8.6zM68.9 26.5H4.2C1.9 26.5 0 28.4 0 30.8c0 2.4 1.9 4.3 4.2 4.3h64.7c2.3 0 4.2-1.9 4.2-4.3C73.2 28.4 71.3 26.5 68.9 26.5zM68.9 53H4.2C1.9 53 0 54.9 0 57.3c0 2.4 1.9 4.3 4.2 4.3h64.7c2.3 0 4.2-1.9 4.2-4.3C73.2 54.9 71.3 53 68.9 53z" />
+                    </svg>
+				</div>
+			</div>
+			<!--
+         -->
+			<div id="items" class="fr-widget fr-container fr_items">
+				<a id="link_1"
+					class="fr-widget fr-text fr-wf fr_navigation_link fr_link_1 fr-widget-hover-opacity-60 fr-link"
+					href="#54abf1a0029e9a0996000011">
+					<div class="fr-text">登录</div>
+				</a>
+				<!--
+             -->
+				<a id="link_2"
+					class="fr-widget fr-text fr-wf fr_navigation_link fr_link_2 fr-widget-hover-opacity-60 fr-link">
+					<div class="fr-text">注册</div>
+				</a>
+				<!--
+             -->
+				<a id="link_3"
+					class="fr-widget fr-text fr-wf fr_navigation_link fr_link_3 fr-widget-hover-opacity-60 fr-link"
+					href="#54abf1a0029e9a0996000011">
+					<div class="fr-text">关于</div>
+				</a>
+			</div>
+		</div>
+		<!--
+     -->
+		<div id="container" class="fr-widget fr-container fr_container">
+			<a href="login.php"><div id="paragraph_center"
+					class="fr-widget fr-text fr-wf fr_paragraph_center">
+					<div class="fr-text">处理订单</div>
+				</div></a>
+		</div>
+	</div>
+	<div class="fr-copyright-footer">
+		<div class="fr-widget fr-container">
+			<div class="fr-widget fr-text fr-wf fr-made-with-froont">
+				<a href="//froont.com" class="fr-text" target="_blank"> MADE WITH <span
+					class="heart">&hearts;</span> AND <b>FROONT</b>
+				</a>
+			</div>
+		</div>
+		<div class="fr-bottom-em-spacer"></div>
+	</div>
+	<div id="footer-assets"></div>
+</body>
+
+</html>
---
+++ login.php
@@ -0,0 +1,142 @@
+<?php
+session_start ();
+echo "start";
+$email = $_POST ['email'];
+$psd = $_POST ['password'];
+echo "values:" . $email . $psd;
+if (isset ( $_SESSION ['userid'] )) {
+	$next_url = "orders.php";
+	header ( 'Location: ' . $next_url );
+} else {
+	$email = $_POST ['email'];
+	$psd = $_POST ['password'];
+	echo $email . $psd;
+	if (! empty ( $email ) && ! empty ( $psd )) {
+		
+		$host = "localhost";
+		$user = "root";
+		$psd = "yangwu";
+		$db = mysql_connect ( $host, $user, $psd );
+		mysql_select_db ( 'wish' );
+		mysql_query ( "set names 'utf-8'" );
+		
+		$query = "select userid from users where email = '".$email."' and psd = '".$psd."'";
+		echo $query;
+		$result = mysql_query ( $query );
+		echo $result;
+		if (mysql_num_rows($result) == 1) {
+			$row = mysqli_fetch_array ( $result );
+			$_SESSION ['userid'] = $row ['userid'];
+			$_SESSION ['email'] = $email;
+			$next_url = "orders.php";
+			header ( 'Location: ' . $next_url );
+		} else {
+			echo "login failed";
+		}
+	} else {
+		echo "please enter valid email and password";
+	}
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Wish 订单助手</title>
+		<meta name="keywords" content="">
+			<link rel="stylesheet" type="text/css"
+				href="https://static.china-merchant.wish.com/china_css/login_page.css?v=6968179812bf" />
+
+</head>
+<body>
+	<!-- HEADER -->
+	<div id="header" class="navbar navbar-fixed-top 
+
+">
+		<div class="container-fluid ">
+			<a class="brand" href="/"> <span class="merchant-header-text">
+
+					wish订单助手 </span>
+			</a>
+
+
+
+
+		</div>
+	</div>
+	<!-- END HEADER -->
+	<!-- SUB HEADER NAV-->
+	<!-- splash page subheader-->
+
+
+
+	<div id="login-page-content" class="center">
+		<form action="login.php" method="post">
+			<div class="clearfix box">
+				<div id="login-form">
+					<div class="header">登录</div>
+					<div class="inputs">
+						<div>
+							<input value="ydengwu@gmail.com" id="email-box" type="text" name="email"
+								class="login-input input-block-level" required="true"
+								placeholder="邮箱地址" />
+						</div>
+						<div>
+							<input value="yangwu19821112" id="password-box" type="password" name="password"
+								class="login-input input-block-level" required="true"
+								placeholder="密码" />
+						</div>
+						<div class="clearfix control-group lst-elem">
+							<div class="pull-left remember-me">
+								<label class="checkbox"> <input checked id="remember-me"
+									type="checkbox" /> 记住我
+								</label>
+							</div>
+							<div class="pull-right">
+								<a class="btn btn-link issue-link pull-right"
+									href=https://china-merchant.wish.com/forget_password>忘记密码了？</a>
+							</div>
+						</div>
+						<input type="submit" value="登录"
+							class="btn btn-large btn-primary btn-block btn-login" />
+
+					</div>
+				</div>
+				<div class="clearfix no-acct-footer">
+					<div class="pull-left">还没有帐户？</div>
+					<div class="pull-right">
+						<a class="btn btn-link issue-link"
+							href=https://china-merchant.wish.com/signup>注册</a>
+
+					</div>
+				</div>
+			</div>
+		</form>
+	</div>
+
+
+
+	</div>
+
+	<!-- global js data -->
+	<script type="text/javascript">
+window.locale_info = {};
+window.locale_info['locale'] = "zh";
+window.locale_info['locale_json'] = null;
+window.pageParams = {"next_url":"\/","monitor_key":"page.login","env":"fe_prod"}
+</script>
+	<script type="text/javascript">
+var lemmings_url="http:\/\/contestimg.wish.com\/api\/webimage";
+</script>
+
+	<!-- end global js data -->
+	<script type="text/javascript" defer async
+		src="https://static.china-merchant.wish.com/build-js-zh-china-cdn/page/login_page.js?v=6968179812bf">
+</script>
+	<!-- END BOTTOM LOAD JS -->
+	<!-- begin user voice code -->
+
+	<!-- end user voice code -->
+</body>
+</html>
