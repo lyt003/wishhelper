d9b1ee9f4745811c9ee789b8f8eb6c15d7e28ce9
yangwu <ydengwu@gmail.com>
2016-01-08 17:14:33 +0800

    compress the image when the size is bigger than 800

--- products.php
+++ products.php
@@ -34,6 +34,43 @@
  * main_image optional URL of a photo for this product variation. Provide this when you have different pictures for different product variation of the product. If left out, it'll use the main_image of the product with the provided parent_sku. Link directly to the image, not the page where it is located. We accept JPEG, PNG or GIF format. Images should be at least 100 x 100 pixels in size.
  *
  */
+ 
+// Function: 获取远程图片并把它保存到本地
+// 确定您有把文件写入本地服务器的权限
+// 变量说明:
+// $url 是远程图片的完整URL地址，不能为空。
+// $filename 是可选变量: 如果为空，本地文件名将基于时间和日期
+// 自动生成.
+function GrabImage($url,$filename="") {
+	if($url==""):return false;endif;
+	if($filename=="") {
+		$ext=strrchr($url,".");
+		if($ext!=".gif" && $ext!=".jpg"):return false;endif;
+		$filename=date("dMYHis").$ext;
+	}
+	ob_start();
+	readfile($url);
+	$img = ob_get_contents();
+	ob_end_clean();
+	$size = strlen($img);
+	$fp2=@fopen($filename, "a");
+	fwrite($fp2,$img);
+	fclose($fp2);
+	return $filename;
+}
+
+function getCompressedImage($sourceURL){
+	list($width, $height, $type) = getimagesize($sourceURL);
+	if($width > 800 || $height > 800){
+		$new=GrabImage($sourceURL,"./images/".basename($sourceURL));
+		//获取压缩该图片文件的地址;
+		@$newURL = "http://www.wishconsole.com/images/".basename($sourceURL)."_800x800.jpg";
+		return $newURL;
+	}
+	return $sourceURL;
+}
+
+
 header ( "Content-Type: text/html;charset=utf-8" );
 include 'Wish/WishClient.php';
 include 'mysql/dbhelper.php';
@@ -76,9 +113,18 @@ if ($productName != null && $description != null && $mainImage != null && $price
 	$productarray ['name'] = $productName;
 	$productarray ['brand'] = $brand;
 	$productarray ['description'] = $description;
-	$productarray ['extra_images'] = $extraImages;
+	
+	$extraImagesArray = explode ( "|", $extraImages );
+	foreach ($extraImagesArray as $extraImage){
+		if($extraImage != null){
+			$productarray ['extra_images'] = $productarray ['extra_images'].getCompressedImage($extraImage).'|';
+		}
+	}
+	//$productarray ['extra_images'] = $extraImages;
+	
 	$productarray ['landingPageURL'] = $landingPageURL;
-	$productarray ['main_image'] = $mainImage;
+	
+	$productarray ['main_image'] =getCompressedImage($mainImage);
 	$productarray ['MSRP'] = $MSRP;
 	$productarray ['price'] = $price;
 	$productarray ['parent_sku'] = $uniqueID;
