e53663a7762aad09a4ba891bd21ed8e1bc985073
yangwu <ydengwu@gmail.com>
2016-05-28 11:51:00 +0800

    add ebay label

--- user/yanwenebay.php
+++ user/yanwenebay.php
@@ -24,6 +24,7 @@ if($result != null){
 		$dbhelper->insertproductLabel ( $currentUserid, "camisole",7);
 		$dbhelper->insertproductLabel ( $currentUserid, "earring",3);
 		$dbhelper->insertproductLabel ( $currentUserid, "sealer",17);
+		$dbhelper->insertproductLabel ( $currentUserid, "cup",16);
 	}
 }
 ?>
